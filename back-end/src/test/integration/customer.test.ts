a1aff9951d84aee482525f94a057604920424544
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 18:12:06 -0300

    test(back): :white_check_mark: Adiciona testes de integração para a rota `GET /customer`

--- back-end/src/test/integration/customer.test.ts
+++ back-end/src/test/integration/customer.test.ts
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
-import { PrismaClient } from '@prisma/client';
+import { Customer, PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 import requester from './utilities/requester';
 
 import {
@@ -11,6 +12,10 @@ const prisma = new PrismaClient();
 
 describe('Test Customer Routes', () => {
   beforeAll(async () => {
+    await prisma.customer.deleteMany();
+  });
+
+  beforeEach(async () => {
     await prisma.customer.deleteMany();
   });
 
@@ -38,6 +43,40 @@ describe('Test Customer Routes', () => {
 
       expect(response.status).toBe(StatusCodes.OK);
       expect(response.body).toEqual([]);
+    });
+  });
+
+  describe('Test GET /customer', () => {
+    afterEach(async () => {
+      await prisma.customer.deleteMany();
+    });
+
+    it('Should return customers filtered by city', async () => {
+      const mockedCustomers = generateMockCustomers(100);
+      const expectedBody = mockedCustomers.filter(
+        ({ city }) => city === mockedCustomers[0].city,
+      );
+
+      await prisma.customer.createMany({ data: mockedCustomers });
+
+      const response = await requester.get(`/customer/?city=${mockedCustomers[0].city}`);
+
+      expect(response.status).toBe(StatusCodes.OK);
+      expect(response.body).toBeInstanceOf(Array);
+      const expectPromises = response.body.map((customer: Customer, idx: number) => (
+        expect(customer).toEqual(expect.objectContaining(expectedBody[idx]))
+      ));
+
+      await Promise.all(expectPromises);
+    });
+
+    it('Should return error when filter is not valid', async () => {
+      const response = await requester.get(`/customer/?first_name=${faker.name.firstName()}`);
+
+      expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+      expect(response.body).toMatchObject({
+        error: expect.any(String),
+      });
     });
   });
 });
