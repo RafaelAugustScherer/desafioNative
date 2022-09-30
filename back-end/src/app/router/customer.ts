2e9e524019eb592db506ff03bd2bef06f0d4c6f1
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 15:30:41 -0300

    feat(back): :sparkles: Adiciona rota `GET /customer` para buscar clientes por cidade
    Busca é feita via query na URL da requisição com a chave `city`

--- back-end/src/app/router/customer.ts
+++ back-end/src/app/router/customer.ts
@@ -2,9 +2,18 @@ import { Router } from 'express';
 import prisma from '../helper/prisma';
 
 import CustomerController from '../controller/customer';
+import CustomerMiddleware from '../middleware/customer';
 
 const customerRouter = Router();
 const ctx = { prisma };
+
+customerRouter.route('/')
+  .get(
+    CustomerMiddleware.validateFilter,
+    (req, res, next) => (
+      CustomerController.readAllByFilter(req, res, next, ctx)
+    ),
+  );
 
 customerRouter.get(
   '/total/by/city',
