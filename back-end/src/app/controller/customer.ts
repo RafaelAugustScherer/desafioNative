053c3d866ef03367c0a175c832eb77ff4cacae23
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 15:26:06 -0300

    feat(back): :sparkles: Adiciona função `readAllByFilter` em `controller/customer`

--- back-end/src/app/controller/customer.ts
+++ back-end/src/app/controller/customer.ts
@@ -2,6 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import CustomerService from '../service/customer';
 import IRequestHandler from '../interface/IRequestHandler';
 
+const readAllByFilter: IRequestHandler = async (req, res, _next, ctx) => {
+  const response = await CustomerService.readAllByFilter({ ...req.query }, ctx);
+
+  res.status(StatusCodes.OK).json(response);
+};
+
 const readTotalCustomersByCity: IRequestHandler = async (_req, res, _next, ctx) => {
   const response = await CustomerService.readTotalCustomersByCity(ctx);
 
@@ -9,5 +15,6 @@ const readTotalCustomersByCity: IRequestHandler = async (_req, res, _next, ctx) 
 };
 
 export default {
+  readAllByFilter,
   readTotalCustomersByCity,
 };
