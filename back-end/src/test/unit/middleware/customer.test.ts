6353009f600afa5e3937da9c6cdb611de6c522d3
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 16:45:33 -0300

    test(back): :white_check_mark: Adiciona testes do middleware `validateFilter` de `customer`

---
+++ back-end/src/test/unit/middleware/customer.test.ts
@@ -0,0 +1,42 @@
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { faker } from '@faker-js/faker';
+import { ValidationError } from 'joi';
+import CustomerMiddleware from '../../../app/middleware/customer';
+import { generateMockCustomer } from '../../shared/customer';
+
+describe('Test Customer Middleware', () => {
+  const req = getMockReq();
+  const { res, next, mockClear } = getMockRes();
+
+  beforeEach(() => {
+    mockClear();
+  });
+
+  describe('Test validateFilter', () => {
+    it('Should correctly validate city filter', async () => {
+      const mockCity = faker.address.cityName();
+      req.query = { city: mockCity };
+
+      await CustomerMiddleware.validateFilter(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('Should not validate other fields', async () => {
+      const mockedCustomer = generateMockCustomer({ withId: true });
+      const fieldsToTest = Object.keys(mockedCustomer).filter((i) => i !== 'city');
+
+      const testPromises = fieldsToTest.map(async (field) => {
+        req.query = { [field]: faker.datatype.string(5) };
+
+        try {
+          await CustomerMiddleware.validateFilter(req, res, next);
+        } catch (e) {
+          expect(e).toBeInstanceOf(ValidationError);
+        }
+        expect(next).not.toHaveBeenCalled();
+      });
+
+      await Promise.all(testPromises);
+    });
+  });
+});
