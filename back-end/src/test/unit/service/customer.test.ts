149d7af2b1b71d0cfcc8058212c854bbe5826370
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 16:15:50 -0300

    test(back): :white_check_mark: Adiciona testes do service `readAllByFilter`

--- back-end/src/test/unit/service/customer.test.ts
+++ back-end/src/test/unit/service/customer.test.ts
@@ -1,4 +1,5 @@
 import { Customer } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 import CustomerService from '../../../app/service/customer';
 import { MockContext, createMockContext } from '../../shared/prisma/context';
 import {
@@ -12,6 +13,28 @@ describe('Test Customer Service', () => {
   beforeEach(() => {
     ctx = createMockContext();
   });
+
+  describe('Test readAllByFilter', () => {
+    it('Should return found customers', async () => {
+      const mockedCustomers = generateMockCustomers(100, { withId: true }) as Customer[];
+      const mockCity = faker.address.cityName();
+
+      ctx.prisma.customer.findMany.mockResolvedValue(mockedCustomers);
+
+      const response = await CustomerService.readAllByFilter({ city: mockCity }, ctx);
+      expect(response).toEqual(mockedCustomers);
+    });
+
+    it('Should return empty array if no customers found', async () => {
+      const mockCity = faker.address.cityName();
+
+      ctx.prisma.customer.findMany.mockResolvedValue([]);
+
+      const response = await CustomerService.readAllByFilter({ city: mockCity }, ctx);
+      expect(response).toEqual([]);
+    });
+  });
+
   describe('Test readTotalCustomersByCity', () => {
     it('Should return total customers by city', async () => {
       const mockedCustomers = generateMockCustomers(100, { withId: true }) as Customer[];
