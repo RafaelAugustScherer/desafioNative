3194614b0f39ba1ac945afbecd78b86d97f9e59a
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-30 15:47:23 -0300

    feat(back): :white_check_mark: Adiciona testes do controller `readAllByFilter`

--- back-end/src/test/unit/controller/customer.test.ts
+++ back-end/src/test/unit/controller/customer.test.ts
@@ -1,5 +1,7 @@
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { StatusCodes } from 'http-status-codes';
+import { Customer } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 import CustomerController from '../../../app/controller/customer';
 import CustomerService from '../../../app/service/customer';
 import { MockContext, createMockContext } from '../../shared/prisma/context';
@@ -16,6 +18,39 @@ describe('Test Customer Controller', () => {
   beforeEach(() => {
     ctx = createMockContext();
     mockClear();
+  });
+
+  describe('Test readAllByFilter', () => {
+    it('Should respond with correct values when customers are found', async () => {
+      const mockedCustomers = generateMockCustomers(100, { withId: true }) as Customer[];
+      const mockedCustomersInCity = mockedCustomers.filter(
+        (customer) => customer.city === mockedCustomers[0].city,
+      );
+      req.query = { city: mockedCustomers[0].city };
+
+      jest.spyOn(CustomerService, 'readAllByFilter')
+        .mockResolvedValue(mockedCustomersInCity);
+
+      await CustomerController.readAllByFilter(req, res, next, ctx);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+      expect(res.json).toHaveBeenCalledWith(
+        expect.arrayContaining(mockedCustomersInCity),
+      );
+    });
+
+    it('Should respond with correct values when there are no customers', async () => {
+      req.query = { city: faker.address.cityName() };
+
+      jest.spyOn(CustomerService, 'readAllByFilter').mockResolvedValue([]);
+
+      await CustomerController.readAllByFilter(req, res, next, ctx);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+      expect(res.json).toHaveBeenCalledWith(
+        expect.arrayContaining([]),
+      );
+    });
   });
 
   describe('Test readTotalCustomersByCity', () => {
