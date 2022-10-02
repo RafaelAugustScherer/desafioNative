a19366d7893d31768953627a41fe72186bfe899d
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-10-01 23:02:35 -0300

    feat(back): :passport_control: Adiciona arquivo helper de autenticação
    Adiciona pacotes `md5, jsonwebtoken` e suas tipagens às dependências

---
+++ back-end/src/app/helper/auth.ts
@@ -0,0 +1,19 @@
+import md5 from 'md5';
+import JWT from 'jsonwebtoken';
+
+const encryptPassword = (password: string) => md5(password);
+
+const generateToken = (payload: object) => {
+  const { JWT_SECRET } = process.env;
+
+  if (!JWT_SECRET) {
+    throw new Error('Please define JWT_SECRET Environment variable');
+  }
+
+  return JWT.sign(payload, JWT_SECRET, { expiresIn: '24h' });
+};
+
+export {
+  encryptPassword,
+  generateToken,
+};
