e892a3455adc19499cff9d88d34fccb7e4f36139
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-10-03 18:07:27 -0300

    feat(front): :sparkles: Cria o componente `CustomerCard` para exibir um cliente em formato de card

---
+++ front-end/src/components/CustomerCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+
+const CustomerCard = ({ customer, color }) => {
+
+  return (
+    <Card sx={{
+      width: '275 px',
+      backgroundColor: color,
+    }}>
+      <CardActionArea sx={{ pb: 3 }}>
+        <CardContent sx={{
+          '& > *': {
+            py: 1,
+          },
+        }}>
+          <Typography color="text.secondary" gutterBottom>
+            # {customer.id}
+          </Typography>
+          <Typography variant="h5">
+            {`${customer.first_name} ${customer.last_name}`}
+          </Typography>
+          <Typography color="text.secondary">
+            {customer.city}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+CustomerCard.propTypes = {
+  customer: PropTypes.shape({
+    id: PropTypes.number,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    email: PropTypes.string,
+    gender: PropTypes.string,
+    company: PropTypes.string,
+    city: PropTypes.string,
+    title: PropTypes.string,
+  }),
+  color: PropTypes.string,
+};
+
+export default CustomerCard;
