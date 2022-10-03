440e5a48e36b810410696acae9fdb5eb34f3e4f3
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-10-03 18:06:15 -0300

    feat(front): :art: Adiciona tema do MaterialUI

--- front-end/src/providers/Theme.js
+++ front-end/src/providers/Theme.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createTheme } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
+import { green, grey } from '@mui/material/colors';
 
 const AppThemeProvider = ({ children }) => {
   const THEME = createTheme({
@@ -9,6 +10,18 @@ const AppThemeProvider = ({ children }) => {
       fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
       h3: {
         marginBottom: '40px',
+      },
+      h4: {
+        margin: '20px 0',
+      },
+    },
+    palette: {
+      primary: {
+        main: green[500],
+      },
+      secondary: {
+        main: green[100],
+        dark: grey[300],
       },
     },
   });
